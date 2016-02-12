aad4399cb9a2f62dd99a644d45319566ae630a35
Honami <honami.725725@gmail.com>
2016-02-11 16:34:57 -1000

    first commit

--- PhotoMaster/ViewController.swift
+++ PhotoMaster/ViewController.swift
@@ -8,11 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @IBOutlet var photoImageView : UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func precentPickerContoroller(sourceType: UIImagePickerControllerSourceType){
+        
+        if UIImagePickerController.isSourceTypeAvailable(sourceType){
+            let picker = UIImagePickerController()
+            picker.sourceType = sourceType
+            picker.delegate = self
+            self.presentViewController(picker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: NSDictionary!) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+        photoImageView.image = image
+    }
+    
+    @IBAction func selectButtonTapped(sender: UIButton){
+        let alertController = UIAlertController(title: "画像の取得先を選択", message: nil, preferredStyle: .ActionSheet)
+        
+        let firstAction = UIAlertAction(title: "カメラ", style: .Default){
+            action in
+            self.precentPickerContoroller(.Camera)
+        }
+        let secondAction = UIAlertAction(title: "アルバム", style: .Default){
+            action in
+            self.precentPickerContoroller(.PhotoLibrary)
+        }
+        let cancelAction = UIAlertAction(title: "キャンセル", style: .Cancel, handler: nil)
+        
+        alertController.addAction(firstAction)
+        alertController.addAction(secondAction)
+        alertController.addAction(cancelAction)
+        
+        presentViewController(alertController, animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
